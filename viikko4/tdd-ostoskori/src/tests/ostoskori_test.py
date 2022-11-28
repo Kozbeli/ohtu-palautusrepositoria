b6f5ba5d1f619ff93933f25df463f9e5cefb7a32
Ossi Ilmanen <ossi.ilmanen@gmail.com>
2022-11-28 23:10:47 +0200

    jos koriin on lisatty tuote ja sama tuote poistetaan on kori taman jalkeen tyhja

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -98,3 +98,10 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_jos_koriin_on_lisaty_tuote_ja_sama_tuote_poistetaan_on_kori_taman_jalkeen_tyhja(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
